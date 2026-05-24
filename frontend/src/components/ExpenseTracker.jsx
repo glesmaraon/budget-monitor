af4058a80873600433b96cdab8ebeefa4bac601f
glesmaraon <gles.maraon@gmail.com>
2026-05-24 23:05:52 +0100

    Update ExpenseTracker V5

--- frontend/src/components/ExpenseTracker.jsx
+++ frontend/src/components/ExpenseTracker.jsx
@@ -1,8 +1,75 @@
 import React from "react";
-export default function Dashboard(){
-return (
+
+export default function ExpenseTracker({
+category,
+setCategory,
+expense,
+setExpense,
+description,
+setDescription,
+date,
+setDate,
+saveExpense
+}){
+
+return(
+
 <div>
-<h1>AI Financial Companion V4</h1>
-<p>Includes reporting, prediction, security layer and reliability metrics.</p>
+
+<h2>💸 Expenses</h2>
+
+<select
+value={category}
+onChange={(e)=>
+setCategory(e.target.value)
+}
+>
+
+<option>Transportation</option>
+<option>Food</option>
+<option>Bills</option>
+<option>Travel</option>
+<option>Shopping</option>
+
+</select>
+
+<br/><br/>
+
+<input
+type="number"
+placeholder="Expense (£)"
+value={expense}
+onChange={(e)=>
+setExpense(e.target.value)
+}
+/>
+
+<br/><br/>
+
+<input
+placeholder="Description"
+value={description}
+onChange={(e)=>
+setDescription(e.target.value)
+}
+/>
+
+<br/><br/>
+
+<input
+type="date"
+value={date}
+onChange={(e)=>
+setDate(e.target.value)
+}
+/>
+
+<button onClick={saveExpense}>
+Save Expense
+</button>
+
 </div>
-)}
+
+)
+
+}
