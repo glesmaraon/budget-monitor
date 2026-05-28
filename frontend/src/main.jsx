7a62f5361892b070fcac36f2a985a5f17cc1bac4
glesmaraon <gles.maraon@gmail.com>
2026-05-28 09:18:01 +0100

    Update main.jsx V9

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
-```jsx id="5m1l1m"
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+```jsx id="h1o0jt"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
 ```
